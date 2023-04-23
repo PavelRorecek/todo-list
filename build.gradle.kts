9d7bbc712ca8a9962f0dec06810dcc1ea5aaf29d
Pavel Rorecek <rorec.pa@gmail.com>
2023-04-23 19:37:23 +0200

    Gradle update

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,8 +9,8 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "8.1.0-alpha11" apply false
-    id("com.android.library") version "8.1.0-alpha11" apply false
+    id("com.android.application") version "8.1.0-beta01" apply false
+    id("com.android.library") version "8.1.0-beta01" apply false
     id("org.jetbrains.kotlin.android") version "1.7.20" apply false
     id("io.gitlab.arturbosch.detekt") version "1.22.0"
 }
